887827d0144d567afdebac6e814bb1e51ffdd4ff
wxamlzm <zd5024@163.com>
2023-10-23 13:59:26 +0800

    UPDATE：在webpack.config.js中配置vue-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/main.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  // 把vue的解析引入
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
   }
 }
