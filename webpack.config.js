8006bb595ff154ca98b0a3d0bec6a0cf0b689280
wxamlzm <zd5024@163.com>
2023-10-24 13:56:39 +0800

    UPDATE：原配置没有能注入vue，更新配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,25 @@
 const path = require('path')
 // 导入 copy 和 html plugin
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-// const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { VueLoaderPlugin } = require('vue-loader')
-const outputPath = path.resolve(__dirname, 'dist'); 
+const outputPath = path.resolve(__dirname, 'dist')
 
 module.exports = {
   entry: './src/main.js',
+
+  // ... 其他配置
+  plugins: [
+    // 根据template里的内容为底，根据entry里的配置注入
+    new HtmlWebpackPlugin({
+      filename: 'index.html', // 输出文件名
+      template: 'public/index.html'
+    }),
+    // 引入vue-loader
+    new VueLoaderPlugin()
+  ],
+
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: outputPath,
     clean: true
   },
@@ -20,25 +31,5 @@ module.exports = {
         loader: 'vue-loader'
       }
     ]
-  },
-  // ... 其他配置
-  plugins: [
-    // 复制 public 文件到输出目录
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, 'public'),
-          to: outputPath
-        }
-      ]
-    }),
-    // 生成空 HTML 文件,不需要内容
-    // new HtmlWebpackPlugin({
-    //   title: 'Hello Vue',
-    //   filename: 'index.html', // 输出文件名
-    //   templateContent: '' // 不生成内容
-    // }),
-    // 引入vue-loader
-    new VueLoaderPlugin()
-  ]
+  }
 }
